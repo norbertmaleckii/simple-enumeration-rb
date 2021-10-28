655aaf363a20ac7ea587de1c8a4d384086b5d70d
Norbert Małecki <norbert.malecki@icloud.com>
2021-10-28 10:49:07 +0200

    Add specs for define simple enumeration with class name

--- spec/simple_enumeration_spec.rb
+++ spec/simple_enumeration_spec.rb
@@ -1,57 +1,113 @@
 # frozen_string_literal: true
 
 RSpec.describe SimpleEnumeration do
-  describe '.has_simple_enumeration' do
-    subject(:order_class) do
-      Class.new do
-        extend SimpleEnumeration
+  describe '.define_simple_enumeration' do
+    context 'without class name' do
+      subject(:order_class) do
+        Class.new do
+          extend SimpleEnumeration
 
-        define_simple_enumeration :payment_status
+          define_simple_enumeration :payment_status
 
-        attr_accessor :payment_status
+          attr_accessor :payment_status
 
-        def initialize(payment_status:)
-          @payment_status = payment_status
+          def initialize(payment_status:)
+            @payment_status = payment_status
+          end
         end
       end
-    end
 
-    let(:payment_status_class) do
-      Class.new(SimpleEnumeration::Entity) do
-        define_basic_collection(
-          :unpaid,
-          :failed,
-          :expired,
-          completed: :paid
-        )
+      let(:payment_status_class) do
+        Class.new(SimpleEnumeration::Entity) do
+          define_basic_collection(
+            :unpaid,
+            :failed,
+            :expired,
+            completed: :paid
+          )
 
-        define_custom_collection(
-          :finished,
-          failed,
-          expired,
-          completed
-        )
+          define_custom_collection(
+            :finished,
+            failed,
+            expired,
+            completed
+          )
+        end
+      end
+
+      before do
+        stub_const('PaymentStatusEnumeration', payment_status_class)
+      end
+
+      it 'defines instance payments status enumeration method' do
+        order = order_class.new(payment_status: 'paid')
+
+        expect(order.payment_status_enumeration).to be_a(PaymentStatusEnumeration)
+        expect(order.payment_status_enumeration.type).to be_a(SimpleEnumeration::Type)
+        expect(order.payment_status_enumeration.type.value).to eql('completed')
+        expect(order.payment_status_enumeration.type.converted_value).to eql('paid')
+
+        order.payment_status = 'failed'
+
+        expect(order.payment_status_enumeration).to be_a(PaymentStatusEnumeration)
+        expect(order.payment_status_enumeration.type).to be_a(SimpleEnumeration::Type)
+        expect(order.payment_status_enumeration.type.value).to eql('failed')
+        expect(order.payment_status_enumeration.type.converted_value).to eql('failed')
       end
     end
 
-    before do
-      stub_const('PaymentStatusEnumeration', payment_status_class)
-    end
+    context 'with class name' do
+      subject(:order_class) do
+        Class.new do
+          extend SimpleEnumeration
 
-    it 'defines instance payments status enumeration method' do
-      order = order_class.new(payment_status: 'paid')
+          define_simple_enumeration :payment_status, with: PaymentStatusEnumeration
 
-      expect(order.payment_status_enumeration).to be_a(PaymentStatusEnumeration)
-      expect(order.payment_status_enumeration.type).to be_a(SimpleEnumeration::Type)
-      expect(order.payment_status_enumeration.type.value).to eql('completed')
-      expect(order.payment_status_enumeration.type.converted_value).to eql('paid')
+          attr_accessor :payment_status
 
-      order.payment_status = 'failed'
+          def initialize(payment_status:)
+            @payment_status = payment_status
+          end
+        end
+      end
 
-      expect(order.payment_status_enumeration).to be_a(PaymentStatusEnumeration)
-      expect(order.payment_status_enumeration.type).to be_a(SimpleEnumeration::Type)
-      expect(order.payment_status_enumeration.type.value).to eql('failed')
-      expect(order.payment_status_enumeration.type.converted_value).to eql('failed')
+      let(:payment_status_class) do
+        Class.new(SimpleEnumeration::Entity) do
+          define_basic_collection(
+            :unpaid,
+            :failed,
+            :expired,
+            completed: :paid
+          )
+
+          define_custom_collection(
+            :finished,
+            failed,
+            expired,
+            completed
+          )
+        end
+      end
+
+      before do
+        stub_const('PaymentStatusEnumeration', payment_status_class)
+      end
+
+      it 'defines instance payments status enumeration method' do
+        order = order_class.new(payment_status: 'paid')
+
+        expect(order.payment_status_enumeration).to be_a(PaymentStatusEnumeration)
+        expect(order.payment_status_enumeration.type).to be_a(SimpleEnumeration::Type)
+        expect(order.payment_status_enumeration.type.value).to eql('completed')
+        expect(order.payment_status_enumeration.type.converted_value).to eql('paid')
+
+        order.payment_status = 'failed'
+
+        expect(order.payment_status_enumeration).to be_a(PaymentStatusEnumeration)
+        expect(order.payment_status_enumeration.type).to be_a(SimpleEnumeration::Type)
+        expect(order.payment_status_enumeration.type.value).to eql('failed')
+        expect(order.payment_status_enumeration.type.converted_value).to eql('failed')
+      end
     end
   end
 
