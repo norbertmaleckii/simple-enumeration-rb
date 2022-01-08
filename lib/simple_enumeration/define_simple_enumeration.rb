1729331f0b5a91fa580b60d49a480c9a4fdbd402
Norbert Małecki <norbert.malecki@icloud.com>
2022-01-08 15:54:32 +0100

    Fix define simple enumeration with option

--- lib/simple_enumeration/define_simple_enumeration.rb
+++ lib/simple_enumeration/define_simple_enumeration.rb
@@ -4,7 +4,7 @@ module SimpleEnumeration
   module DefineSimpleEnumeration
     def define_simple_enumeration(attribute, options = {})
       enum_class_name = options[:with] || enum_class_name_for(attribute)
-      enum_class = const_get(enum_class_name)
+      enum_class = enum_class_name.is_a?(String) ? const_get(enum_class_name) : enum_class_name
 
       define_method "#{attribute}_enumeration" do
         enum_class.new(converted_value: send(attribute))
